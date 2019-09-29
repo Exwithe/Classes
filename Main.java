47de587cc692a37ef65e9010344df562cb1619ee
Exwithe <52176228+Exwithe@users.noreply.github.com>
2019-09-29 20:01:48 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Person marvin = new Person("Marvin", 174,122, "Janina"); new Work("Software developer", 1300, 3);
-        Person res = new Person("Res", 186, 99, "Sarina"); new Work("electrician", 4800, 2);
+        Person marvin = new Person("Marvin", 174,122, "Janina", new Work ("Software developer", 1300, 3));
+        Person res = new Person("Res", 186, 99, "Sarina") new Work("electrician", 4800, 2);
         Person remo = new Person("Remo", 172,118,"Error404"); new Work ("electrician", 4800, 2);
 
 
 
 
         System.out.println(marvin.getName() + " is " + marvin.getSize() + " cm tall. His IQ is " + marvin.getIq() + " and his girlfriend is called " + marvin.getGirlFriendsName() + ".");
+        System.out.println(marvin.getWork());
     }
 }
